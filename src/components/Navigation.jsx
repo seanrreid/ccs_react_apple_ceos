522c6770433a27665d42efbb3bf550bfa880eed4
Sean R Reid <sean@torchcodelab.com>
2024-06-26 08:20:34 -0400

    Added class to nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,24 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navigation = ({ navItems }) => {
-    return (
-        <nav>
-            <ul>
-                {navItems.map((link, index) => {
-                    return (
-                        <li key={`${link.title}-${index}`}>
-                            <Link to={link.url}>{link.title}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <ul className="flex">
+        {navItems.map((link, index) => {
+          return (
+            <li key={`${link.title}-${index}`}>
+              <Link to={link.url}>{link.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 Navigation.propTypes = {
-    navItems: PropTypes.array,
-}
+  navItems: PropTypes.array,
+};
 
 export default Navigation;
